e5d7f784e09f6647c23367f6891050a588d04372
Family Galvez <familygalvez@macmini.lan>
2025-08-31 14:59:27 -0700

    Initial commit: Basic SwiftUI project structure

---
+++ Centered/CenteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenteredApp.swift
+//  Centered
+//
+//  Created by Family Galvez on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CenteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
